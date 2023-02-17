8ae36292b0cf1e9bba4400172e4194a7ff06e1d8
Justin Kuhn <justintkuhn@gmail.com>
2023-02-17 14:29:06 +0000

    the first panel is on

--- src/pages/Freelance.vue
+++ src/pages/Freelance.vue
@@ -1,11 +1,12 @@
 <template>
     <Layout>
         <div id="three-container">
-            <!-- The ref attribute allows us to reference this element: this.$refs.canvas -->
-            <canvas ref="canvas"></canvas>
         </div>
         <div id="2d-container">
-
+            <div id="2nd-side"> 
+                I’m a freelance <br>creative technologist<br>
+                 working with<br> 3D, audio, and XR
+            </div>
         </div>
     </Layout>
 </template>
@@ -27,9 +28,12 @@
         DirectionalLight,
         BufferGeometry,
     } from "three";
+    import { CSS3DObject, CSS3DRenderer } from "three/examples/jsm/renderers/CSS3DRenderer"
 
     export default {
         mounted() {
+
+
             const scene = new Scene();
             // Determines the position from which the user "sees"
             const camera = new PerspectiveCamera(
@@ -39,23 +43,38 @@
             1000
             );
             // Converts 3D to 2D via calculations
-            const renderer = new WebGLRenderer({
-            canvas: this.$refs.canvas
-            });
+            const renderer = new CSS3DRenderer();
             renderer.setSize( window.innerWidth, window.innerHeight );
-            document.body.appendChild( renderer.domElement );
-            const geometry = new BoxGeometry( 1, 1, 1 );
-            const material = new MeshBasicMaterial( { color: 0x00ff00 } );
-            const cube = new Mesh( geometry, material );
-            scene.add( cube );
+            document.getElementById( 'three-container' ).appendChild( renderer.domElement );
 
-            camera.position.z = 5;
+            const element = document.createElement( 'div' );
+            element.className = 'panel';
+            element.style.backgroundColor = 'rgba(0,127,127,' + ( Math.random() * 0.5 + 0.25 ) + ')';
+
+            const details = document.createElement( 'div' );
+            details.className = 'details';
+            details.innerHTML = document.getElementById("2nd-side").innerHTML ;
+            element.appendChild( details );
+
+            const objectCSS = new CSS3DObject( element );
+            objectCSS.position.x = 0;
+            objectCSS.position.y = 0;
+            objectCSS.position.z = -5;
+            scene.add( objectCSS );
+
+
+            // const geometry = new BoxGeometry( 1, 1, 1 );
+            // const material = new MeshBasicMaterial( { color: 0x00ff00 } );
+            // const cube = new Mesh( geometry, material );
+            // scene.add( cube );
+
+            camera.position.z = 500;
 
             function animate() {
 				requestAnimationFrame( animate );
 
-				cube.rotation.x += 0.01;
-				cube.rotation.y += 0.01;
+				// cube.rotation.x += 0.01;
+				// cube.rotation.y += 0.01;
 
 				renderer.render( scene, camera );
 			};
@@ -65,6 +84,32 @@
     }
 </script>
 
-<style scoped>
+<style>
+
+.panel {
+    width: 200px;
+    height: 200px;
+    box-shadow: 0px 0px 12px rgba(0,255,255,0.5);
+    border: 1px solid rgba(127,255,255,0.25);
+    font-family: Helvetica, sans-serif;
+    text-align: center;
+    line-height: normal;
+    cursor: default;
+}
+
+.panel:hover {
+    box-shadow: 0px 0px 12px rgba(0,255,255,0.75);
+    border: 1px solid rgba(127,255,255,0.75);
+}
+
+.panel .details {
+    position: absolute;
+    bottom: 15px;
+    left: 0px;
+    right: 0px;
+    font-size: 20px;
+    text-align: center;
+    color: rgba(127,255,255,0.75);
+}
 
 </style>
--- text/todo.md
+++ text/todo.md
@@ -1,4 +1,6 @@
-choose 1st or 3rd person voice
-artist statement
 filter by tags
 
+freelance page
+- 3d/2d toggle
+- 6 planes with HTML 
+- 
